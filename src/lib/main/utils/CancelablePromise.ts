e6d69074416b47911b3b83337298eaae617c043b
Luca Scaringella <lucalstyle@gmail.com>
2020-12-08 00:12:45 +0100

    Implement lightweight cancelable promise which can be connected to an existing promise.

---
+++ src/lib/main/utils/CancelablePromise.ts
@@ -0,0 +1,44 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import {Writable} from "./Types";
+
+export class CancellationError extends Error {
+    constructor(message: string = 'The promise was cancelled.') {
+        super(message);
+    }
+}
+
+export interface CancelablePromise<T> extends Promise<T> {
+    readonly canceled: boolean
+    cancel(reason?: any): boolean
+}
+
+export function toCancelablePromise<T>(promise: Promise<T>,canceler: () => boolean): CancelablePromise<T> {
+    let promiseDone: boolean = false;
+    let promiseRej: (reason: any) => void;
+    const p = new Promise((res,rej) => {
+        promise.then((result) => {
+            promiseDone = true;
+            res(result);
+        }).catch((err) => {
+            promiseDone = true;
+            rej(err);
+        });
+        promiseRej = rej;
+    }) as CancelablePromise<T>;
+    (p as Writable<CancelablePromise<T>>).canceled = false;
+    p.cancel = (reason: any = new CancellationError()) => {
+        if(promiseDone || p.canceled) return p.canceled;
+        if(canceler()) {
+            (p as Writable<CancelablePromise<T>>).canceled = true;
+            promiseRej(reason);
+            return true;
+        }
+        return false;
+    }
+    return p;
+}
