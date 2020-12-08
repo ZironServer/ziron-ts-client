28cc561f736d9e52692a65177583dc0a6fe89747
Luca Scaringella <lucalstyle@gmail.com>
2020-12-08 22:51:59 +0100

    Define Socket options.

---
+++ src/lib/core/SocketOptions.ts
@@ -0,0 +1,48 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import {ClientOptions as WSClientOptions} from "ws";
+import TokenStore from "../main/tokenStore/TokenStore";
+
+export interface AutoReconnectOptions {
+    /**
+     * @default true
+     */
+    active?: boolean,
+    initialDelay?: number,
+    randomness?: number,
+    multiplier?: number,
+    maxDelay?: number | null
+}
+
+export default interface SocketOptions {
+    hostname?: string,
+    port?: number,
+    secure?: boolean,
+    path?: string,
+    autoReconnect?: AutoReconnectOptions,
+    autoSubscribeOnConnect?: boolean,
+    connectTimeout?: number,
+    ackTimeout?: number,
+    transmitSendTimeout?: null | number;
+    invokeSendTimeout?: null | number;
+    handshakeAttachment?: any,
+    wsOptions?: WSClientOptions,
+    /**
+     * Specifies the token store that is used to save/load and remove the token.
+     * Internal the client will also store the token in a variable
+     * independent of what store is used.
+     * The stored token will be loaded whenever the client wants to connect again.
+     * The load tries to load the token with the store if no store is provided
+     * or the load fails it will load the token from the in-memory variable.
+     * The client already provides a local storage token store that
+     * uses the local storage of a browser.
+     * This store helps to reload the signed token when the client
+     * opens a new Tab or reloads the site in the browser.
+     * @default undefined
+     */
+    tokenStore?: TokenStore | null
+}
