035d3875225a92784b1fb56e071d195a4ecf3190
Luca Scaringella <lucalstyle@gmail.com>
2021-10-03 17:23:50 +0200

    Export toCancelablePromise function.

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import Socket, {UnsubscribeReason, ProcedureListener, ReceiverListener} from "./
 import TokenStore                            from "./lib/main/tokenStore/TokenStore";
 import {createLocalStorageTokenStore}        from "./lib/main/tokenStore/LocalStorageTokenStore";
 import SocketOptions                         from "./lib/core/SocketOptions";
-import {CancelablePromise,CancellationError} from "./lib/main/utils/CancelablePromise";
+import {CancelablePromise, CancellationError, toCancelablePromise} from "./lib/main/utils/CancelablePromise";
 import {TimeoutError, Transport}             from "ziron-engine";
 
 EventEmitter.onceTimeoutErrorCreator = () => new TimeoutError('Once timeout reached.','OnceListener');
@@ -21,6 +21,7 @@ export {
     SocketOptions,
     TokenStore,
     createLocalStorageTokenStore,
+    toCancelablePromise,
     CancelablePromise,
     CancellationError,
     prepareMultiTransmit,
