5cb78c1543f5814bac154fd51abec3b22e540717
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-06-19 15:23:11 +0200

    Implement Deferred fulfilled property.

--- src/lib/main/utils/Deferred.ts
+++ src/lib/main/utils/Deferred.ts
@@ -7,10 +7,17 @@ Copyright(c) Ing. Luca Gian Scaringella
 import {Writable} from "./Types";
 
 export default class Deferred<T = any> {
+    public readonly fulfilled: boolean = false;
     public readonly resolve: (data: T) => void;
     public readonly reject: (err: any) => void;
     public readonly promise: Promise<T> = new Promise<T>((res,rej) => {
-        (this as Writable<Deferred<T>>).resolve = res;
-        (this as Writable<Deferred<T>>).reject = rej;
+        (this as Writable<Deferred<T>>).resolve = (result) => {
+            (this as Writable<Deferred<T>>).fulfilled = true;
+            res(result);
+        };
+        (this as Writable<Deferred<T>>).reject = (err) => {
+            (this as Writable<Deferred<T>>).fulfilled = true;
+            rej(err);
+        };
     })
 }
